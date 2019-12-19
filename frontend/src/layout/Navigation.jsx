ae11557059c88f63d0e4181de032217a7984a352
F34th3R <jafetlopezch@gmail.com>
2019-12-19 17:08:27 -0600

    more btn in navigation components === isWork!! yeah bi...bit

--- frontend/src/layout/Navigation.jsx
+++ frontend/src/layout/Navigation.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import { LogoLarge } from '../components/LogoLarge'
+import { LinkComponent } from '../components/LinkComponent'
 
 export const Navigation = () => {
+  const [selectMenu, setSelectMenu] = useState(false)
   const StyeledHeader = styled.header`
     width: 100%;
     height: 66px;
@@ -34,14 +36,56 @@ export const Navigation = () => {
     }
   `
 
+  const StyledMore = styled.div`
+    z-index: 3;
+    display: ${props => (props.display ? 'flex' : 'none')};
+    flex-direction: column;
+    position: absolute;
+    right: 0;
+    top: 56px;
+    min-width: 145px;
+    background: #f0f2f0;
+    border-radius: 5px;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.07);
+    padding: 8px 0;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: -12px;
+      width: 0;
+      height: 0;
+      right: 12px;
+      border-bottom: 12px solid #f0f2f0;
+      border-left: 12px solid transparent;
+      border-right: 12px solid transparent;
+    }
+    div {
+      padding: 10px 18px;
+      span {
+        width: 100%;
+        min-height: 22px;
+        cursor: pointer;
+        font-weight: 600;
+      }
+    }
+  `
+
   return (
-    <StyeledHeader>
-      <div>
-        <LogoLarge />
-      </div>
-      <div>
-        <MoreVertIcon />
-      </div>
-    </StyeledHeader>
+    <>
+      <StyeledHeader>
+        <div>
+          <LogoLarge />
+        </div>
+        <div onClick={() => setSelectMenu(prev => !prev)}>
+          <MoreVertIcon />
+        </div>
+      </StyeledHeader>
+      <StyledMore display={selectMenu}>
+        <LinkComponent to="/login">
+          <span>LogOut</span>
+        </LinkComponent>
+      </StyledMore>
+    </>
   )
 }
